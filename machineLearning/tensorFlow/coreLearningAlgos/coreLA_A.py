ad8fc4476cd224cd6fd3e43347076b2d35fe048c
Chase <cnickellsumn@gmail.com>
2022-12-08 08:55:13 -0800

    asdf

--- machineLearning/tensorFlow/coreLearningAlgos/coreLA_A.py
+++ machineLearning/tensorFlow/coreLearningAlgos/coreLA_A.py
@@ -1,16 +1,17 @@
 from  __future__ import absolute_import, division, print_function, unicode_literals
 
-import sys
-sys.path
+# import sys
+# sys.path
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from IPython.display import clear_output
 from six.moves import urllib
 
-import tensorFlow.compat.v2.feature_column as fc
 
+import tensorFlow.compat.v2.feature_column as fc
 import tensorFlow as tf
+
 
 # Load dataset.
 dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv') # training data
