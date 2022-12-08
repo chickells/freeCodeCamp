4eb4497a8938d06db2c8eb27030f0e3f2375bded
Chase <cnickellsumn@gmail.com>
2022-12-08 08:59:59 -0800

    IT WORKS SLUT
    literally just c&p'd the top import text (which i manually entered earlier)
    and for some reason that fixed it, must have had a typo lol

--- machineLearning/tensorFlow/coreLearningAlgos/coreLA_A.py
+++ machineLearning/tensorFlow/coreLearningAlgos/coreLA_A.py
@@ -1,21 +1,21 @@
-from  __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import absolute_import, division, print_function, unicode_literals
 
-# import sys
-# sys.path
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from IPython.display import clear_output
 from six.moves import urllib
 
+import tensorflow.compat.v2.feature_column as fc
 
-import tensorFlow.compat.v2.feature_column as fc
-import tensorFlow as tf
-
+import tensorflow as tf
+# import sys
+# sys.path
 
 # Load dataset.
 dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv') # training data
 dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv') # testing data
-dftrain.head()
+print(dftrain.head())
 y_train = dftrain.pop('survived')
 y_eval = dfeval.pop('survived')
+print(dftrain.head())
