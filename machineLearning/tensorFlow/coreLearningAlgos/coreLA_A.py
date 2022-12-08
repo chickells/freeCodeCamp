3a17817e33b584d706d2e6d9a5b3191c01101924
Chase <cnickellsumn@gmail.com>
2022-12-08 08:50:49 -0800

    installed pandas and tensorFlow

--- machineLearning/tensorFlow/coreLearningAlgos/coreLA_A.py
+++ machineLearning/tensorFlow/coreLearningAlgos/coreLA_A.py
@@ -1,5 +1,7 @@
 from  __future__ import absolute_import, division, print_function, unicode_literals
 
+import sys
+sys.path
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -10,3 +12,9 @@ import tensorFlow.compat.v2.feature_column as fc
 
 import tensorFlow as tf
 
+# Load dataset.
+dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv') # training data
+dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv') # testing data
+dftrain.head()
+y_train = dftrain.pop('survived')
+y_eval = dfeval.pop('survived')
